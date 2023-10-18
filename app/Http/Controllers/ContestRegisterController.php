9a7ea76479af9fe3190065e9e1e35ad337434c6b
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2023-10-18 09:33:16 +0700

    create model dan database

---
+++ app/Http/Controllers/ContestRegisterController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ContestRegister;
+use Illuminate\Http\Request;
+
+class ContestRegisterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ContestRegister $contestRegister)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ContestRegister $contestRegister)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ContestRegister $contestRegister)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ContestRegister $contestRegister)
+    {
+        //
+    }
+}
